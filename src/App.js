4175848ae7649b8bf09b2fb4d672deee5cb786d5
JirapunJunruk <jirapun0907@gmail.com>
2023-09-08 15:33:54 +0700

    context-content header and header 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import Banner from './banner';
-import {Calculator} from './calculator';
-import { EventData2 } from './event-data';
-import RefsFunc from './refs-func';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
+// import { Content, Footer, Header } from './func-components';
+// import Calendar from './class-components';
+// import Banner from './banner';
+// import {Calculator} from './calculator';
+// import { EventData2 } from './event-data';
+// import RefsFunc from './refs-func';
+// import RefsArray from './refs-array';
+// import MessageBox from './state-func';
+   import { userContext } from './context';
+// import Header from './context-header';
+// import Content from './context-content';
+   import Header2 from './context-header2';
+   import Content2 from './context-content2';
 
 
-function App() {
+
+export default function App() {
+  let [user,setUser] = React.useState('')
   return (
-   <MessageBox/>
+   <userContext.Provider value = {[user,setUser]}>
+    <Header2/>
+    <Content2/>
+    </userContext.Provider>
   )
 }
-export default App;
