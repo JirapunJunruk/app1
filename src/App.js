074d182de63e420e410ac96331299cbeabacab10
JirapunJunruk <jirapun0907@gmail.com>
2023-09-22 14:42:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import Banner from './banner';
-import {Calculator} from './calculator';
-import { EventData2 } from './event-data';
-import RefsFunc from './refs-func';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
+//import { Content, Footer, Header } from './func-components';
+//import Calendar from './class-components';
+//import Banner from './banner';
+//import {Calculator} from './calculator';
+//import { EventData2 } from './event-data';
+//import RefsFunc from './refs-func';
+//import RefsArray from './refs-array';
+//import MessageBox from './state-func';
+import Router2 from './router2';
 
 
 function App() {
   return (
-   <MessageBox/>
+    <Router2/>
+    
   )
 }
 export default App;
